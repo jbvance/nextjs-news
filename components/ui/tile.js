c774850bcb6f6cab2c4a701f426700d009212c7c
Jason Vance <jbvance@gmail.com>
2021-09-24 18:27:20 -0500

    add tile components and styles

--- components/ui/tile.js
+++ components/ui/tile.js
@@ -1,35 +1,50 @@
-import {
-  subtleBoxShadow,
-  lightBlueBackground,
-  greenBoxShadow,
-  redBoxShadow
-} from '../../styles/Styles';
+import classes from './tile.module.css';
 
-export const Tile = styled.div`
-  ${subtleBoxShadow}
-  ${lightBlueBackground}
-    padding: 10px;
-`;
+export const Tile = (props) => {
+  return <div className={classes.tile}>{props.children}</div>;
+};
 
-export const SelectableTile = styled(Tile)`
-  &:hover {
-    cursor: pointer;
-    ${greenBoxShadow}
-  }
-`;
+export const SelectableTile = (props) => {
+  return <div className={classes.selectableTile}>{props.children}</div>;
+};
 
-export const DeletableTile = styled(SelectableTile)`
-  &:hover {
-    cursor: pointer;
-    ${redBoxShadow}
-  }
-`;
+export const DeletableTile = (props) => {
+  return <div className={classes.deletableTile}>{props.children}</div>;
+};
 
-export const DisabledTile = styled(Tile)`
-  pointer-events: none;
-  opacity: 0.4;
-`;
+export const DisabledTile = (props) => {
+  return <div className={classes.disabledTile}>{props.children}</div>;
+};
 
-export const SelectedTile = styled(Tile)`
-  ${greenBoxShadow}
-`;
+export const SelectedTile = (props) => {
+  return <div className={classes.selectedTile}>{props.children}</div>;
+};
+
+// export const Tile = styled.div`
+//   ${subtleBoxShadow}
+//   ${lightBlueBackground}
+//     padding: 10px;
+// `;
+
+// export const SelectableTile = styled(Tile)`
+//   &:hover {
+//     cursor: pointer;
+//     ${greenBoxShadow}
+//   }
+// `;
+
+// export const DeletableTile = styled(SelectableTile)`
+//   &:hover {
+//     cursor: pointer;
+//     ${redBoxShadow}
+//   }
+// `;
+
+// export const DisabledTile = styled(Tile)`
+//   pointer-events: none;
+//   opacity: 0.4;
+// `;
+
+// export const SelectedTile = styled(Tile)`
+//   ${greenBoxShadow}
+// `;
--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,9 +1,13 @@
 import { getSession } from 'next-auth/client';
+import { SelectableTile } from '../components/ui/tile';
 
 const Dashboard = () => {
   return (
     <div>
       <h1>Welcome!</h1>
+      <SelectableTile>
+        <div>This is a tile</div>
+      </SelectableTile>
     </div>
   );
 };
@@ -15,13 +19,13 @@ export async function getServerSideProps(context) {
     return {
       redirect: {
         destination: '/auth',
-        permanent: false
-      }
+        permanent: false,
+      },
     };
   }
 
   return {
-    props: { session }
+    props: { session },
   };
 }
 
